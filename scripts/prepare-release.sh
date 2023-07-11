05a373579f22584bf7800d19e3290b5e3c7d97cb
possumbilities <109087089+possumbilities@users.noreply.github.com>
2023-07-11 22:13:35 +0000

    setup basic file to work from

---
+++ scripts/prepare-release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -o errexit
+set -o errtrace
+set -o nounset
+
+# always run this in the project's root
+
+git checkout -b prep-${1}
+mv ./src/* ./
+rm -r ./src
+# remove other files here
+echo "prep complete";
+git status
+echo 'please remove the scripts folder to continue'
+echo "rm -r ./scripts"
+echo "changes ready to be commited, please commit, and push with'
+echo "git push origin prep-${1}"
