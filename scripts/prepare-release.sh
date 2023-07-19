f3d6a4a640ed0af66f77389cbe78415d652c11bd
Timid Robot Zehta <timid@creativecommons.org>
2023-07-19 07:30:27 -0700

    increase verbosity (and hopefully clarity)

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -6,6 +6,7 @@ set -o nounset
 # setup fun colors for added UX
 BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
+RED='\033[0;32m'
 NC='\033[0m'
 
 # Change directory to repository root
@@ -14,11 +15,18 @@ pushd "${0%/*}/.." >/dev/null
 
 if [[ -z "${1+x}" ]]
 then
-     echo -e "${BLUE}missing VERSION argument, in format: v0.1.0${NC}" 1>&2
+     {
+        echo
+        echo -e "${RED}missing VERSION argument, in format: v0.1.0${NC}"
+        echo
+     } 1>&2
      exit 1
 else
     printf "\e[1m\e[7m %-80s\e[0m\n" 'Checkout prep branch'
     git checkout -b "prep-${1}"
+    echo
+
+    printf "\e[1m\e[7m %-80s\e[0m\n" 'Stage directories/files for release'
     mv ./src/* ./
     # remove unneeded files for release (and self destruct)
     rm -fr -- \
@@ -32,10 +40,16 @@ else
         .gitignore \
         docker-compose.yml
 
-    echo 'prep complete';
-    git status
+    echo 'done.'
+    echo
 
+    printf "\e[1m\e[7m %-80s\e[0m\n" 'Repository status'
+    git status
+    echo
+
+    printf "\e[1m\e[7m %-80s\e[0m\n" 'Next steps'
     echo -en "${BLUE}changes ready to be commited, please commit, and push"
-    echo -e " with${NC}"
-    echo -e "${PURPLE}git push origin prep-${1}${NC}"
+    echo -e " with:${NC}"
+    echo -e "    ${PURPLE}git push origin prep-${1}${NC}"
+    echo
 fi
