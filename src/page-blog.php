776c32a6721bb6b475172c9efc850b5ef4dabe58
possumbilities <109087089+possumbilities@users.noreply.github.com>
2023-08-18 20:56:51 +0000

    incorporate newest blog-index context changes

--- src/page-blog.php
+++ src/page-blog.php
@@ -6,6 +6,8 @@
 
 <h1>Blog</h1>
 
+<!-- <span class="byline">by <a href="#">Brigitte Vezina</a>, <a href="#">Ony Anukem</a></span> -->
+
 <p>lead in paragraph</p>
 
 <!-- <span class="categories">
@@ -17,12 +19,209 @@
 
 </header>
 
+<article class="stories authored-posts highlight">
 
-<article class="featured news">
+    
+    <article class="story">
+        <header>
+        <h2 class="title"><a href="#">Open Access in Practice: A Conversation with President Larry Kramer of The Hewlett Foundation</a></h2>
+    
+        <span class="byline">by <a href="#">Brigitte Vezina</a>, <a href="#">Ony Anukem</a></span>
+        <span class="categories"><a href=#">Open Culture</a></span>
+        </header>
+
+        <figure>
+            <img src="../imgs/image2.jpg" />
+            <span class="attribution"><span>"</span><a href="https://www.flickr.com/photos/47691521@N07/8249753855" target="_blank" rel="noopener noreferrer">Creative Commons a vessel ideas</a><span>" by&nbsp;</span><a href="https://www.flickr.com/photos/47691521@N07" target="_blank" rel="noopener noreferrer">opensourceway</a><span>&nbsp;is licensed under&nbsp;</span><a href="https://creativecommons.org/licenses/by-sa/2.0/?ref=openverse" target="_blank" rel="noopener noreferrer">CC BY-SA 2.0</a></span>
+        </figure>
+        <p>The Creative Commons Open Education Team is pleased to provide a snapshot of progress made toward opening access and equity in education, through a look at our collective efforts in 2022.1 We laud the CC open education community for its important work throughout 2022. CC and community members' open education efforts in 2022 included, but </p>
+        
+    </article>
+
+    <article class="story">
+        <header>
+        <h2 class="title"><a href="#">Open Access in Practice: A Conversation with President Larry Kramer of The Hewlett Foundation</a></h2>
+        <span class="byline">by <a href="#">Brigitte Vezina</a>, <a href="#">Ony Anukem</a></span>
+        <span class="categories"><a href=#">Open Culture</a></span>
+    
+        </header>
+    
+        <figure>
+            <img src="../imgs/image3.png" />
+            
+            <!-- <span class="attribution">"<a href="https://thegreats.co/artworks/the-more-we-share-the-more-we-have-series-22">The More We Share, The More We Have (series 1/2)</a>" by <a href="https://thegreats.co/artists/pietro-soldi">Pietro Soldi</a> for Creative Commons &amp; Fine Acts is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a></span> -->
+        </figure>
+       
+    </article>
+    
+    <article class="story">
+        <header>
+       <h2 class="title"><a href="#">Open Access in Practice: A Conversation with President Larry Kramer of The Hewlett Foundation</a></h2>
+        <span class="byline">by <a href="#">Brigitte Vezina</a>, <a href="#">Ony Anukem</a></span>
+        <span class="categories"><a href=#">Open Culture</a></span>
+    
+    </header>
+    
+        <figure>
+            <img src="../imgs/image4.png" />
+            
+            <!-- <span class="attribution">"<a href="https://thegreats.co/artworks/the-more-we-share-the-more-we-have-series-22">The More We Share, The More We Have (series 1/2)</a>" by <a href="https://thegreats.co/artists/pietro-soldi">Pietro Soldi</a> for Creative Commons &amp; Fine Acts is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a></span> -->
+        </figure>
+       
+    </article>
+
+    <article class="story">
+        <header>
+       <h2 class="title"><a href="#">Open Access in Practice: A Conversation with President Larry Kramer of The Hewlett Foundation</a></h2>
+        <span class="byline">by <a href="#">Brigitte Vezina</a>, <a href="#">Ony Anukem</a></span>
+        <span class="categories"><a href=#">Open Culture</a></span>
+    
+    </header>
+    
+        <figure>
+            <img src="../imgs/image3.png" />
+            
+            <!-- <span class="attribution">"<a href="https://thegreats.co/artworks/the-more-we-share-the-more-we-have-series-22">The More We Share, The More We Have (series 1/2)</a>" by <a href="https://thegreats.co/artists/pietro-soldi">Pietro Soldi</a> for Creative Commons &amp; Fine Acts is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a></span> -->
+        </figure>
+       
+    </article>
+
+    <article class="story">
+        <header>
+       <h2 class="title"><a href="#">Open Access in Practice: A Conversation with President Larry Kramer of The Hewlett Foundation</a></h2>
+        <span class="byline">by <a href="#">Brigitte Vezina</a>, <a href="#">Ony Anukem</a></span>
+        <span class="categories"><a href=#">Open Culture</a></span>
+    
+    </header>
+    
+        <figure>
+            <img src="../imgs/image3.png" />
+            
+            <!-- <span class="attribution">"<a href="https://thegreats.co/artworks/the-more-we-share-the-more-we-have-series-22">The More We Share, The More We Have (series 1/2)</a>" by <a href="https://thegreats.co/artists/pietro-soldi">Pietro Soldi</a> for Creative Commons &amp; Fine Acts is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a></span> -->
+        </figure>
+       
+    </article>
+
+    <footer>
+        <article class="attribution-list">
+        <h2>Images Attribution</h2>
+            <ul>
+                <li>
+                    <article>
+                        <figure>
+                            <img src="../imgs/image3.png" />
+                            <span class="attribution">"<a href="https://thegreats.co/artworks/the-more-we-share-the-more-we-have-series-22">The More We Share, The More We Have (series 1/2)</a>" by <a href="https://thegreats.co/artists/pietro-soldi">Pietro Soldi</a> for Creative Commons &amp; Fine Acts is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a></span>
+                        </figure>
+                    </article>
+                </li>
+                <li>
+                    <article>
+                        <figure>
+                            <img src="../imgs/image4.png" />
+                            <span class="attribution"><span>"</span><a href="https://www.flickr.com/photos/47691521@N07/8249753855" target="_blank" rel="noopener noreferrer">Creative Commons a vessel ideas</a><span>" by&nbsp;</span><a href="https://www.flickr.com/photos/47691521@N07" target="_blank" rel="noopener noreferrer">opensourceway</a><span>&nbsp;is licensed under&nbsp;</span><a href="https://creativecommons.org/licenses/by-sa/2.0/?ref=openverse" target="_blank" rel="noopener noreferrer">CC BY-SA 2.0</a></span>
+                        </figure>
+                    </article>
+                </li>
+                <li>
+                    <article>
+                        <figure>
+                            <img src="../imgs/image3.png" />
+                            <span class="attribution">"<a href="https://thegreats.co/artworks/the-more-we-share-the-more-we-have-series-22">The More We Share, The More We Have (series 1/2)</a>" by <a href="https://thegreats.co/artists/pietro-soldi">Pietro Soldi</a> for Creative Commons &amp; Fine Acts is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a></span>
+                        </figure>
+                    </article>
+                </li>
+                <li>
+                    <article>
+                        <figure>
+                            <img src="../imgs/image3.png" />
+                            <span class="attribution">"<a href="https://thegreats.co/artworks/the-more-we-share-the-more-we-have-series-22">The More We Share, The More We Have (series 1/2)</a>" by <a href="https://thegreats.co/artists/pietro-soldi">Pietro Soldi</a> for Creative Commons &amp; Fine Acts is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a></span>
+                        </figure>
+                    </article>
+                </li>
+            </ul>
+        </article>
+    </footer>
+    
+    
 
 </article>
 
+<article class="stories authored-posts">
+    <h2>Recent Posts</h2>
+    
+    <article class="story">
+        <header>
+        <h3 class="title"><a href="#">Introducing Our Google Season of Docs 2020 Participants</a></h3>
+        <span class="byline">by <a href="#">Brigitte Vezina</a>, <a href="#">Ony Anukem</a></span>
+        <span class="categories"><a href=#">Open Knowledge</a></span>
+    
+        </header>
+        <p>In search of answers, we looked at past research, notably Andrea Wallace's Barriers to Open Access — Open GLAM, and asked more than 30 experts in the open culture movement. You can watch what they told us in our CC Open Culture VOICES vlog series. Here's a small sample of what we heard</p>
+    </article>
 
+    <article class="story">
+        <header>
+        <h3 class="title"><a href="#">Open Access in Practice: A Conversation with President Larry Kramer of The Hewlett Foundation</a></h3>
+        <span class="byline">by <a href="#">Brigitte Vezina</a>, <a href="#">Ony Anukem</a></span>
+        <span class="categories"><a href=#">Open Culture</a></span>
+        </header>
+
+        <figure>
+            <img src="../imgs/image3.png" />
+            
+            <span class="attribution">"<a href="https://thegreats.co/artworks/the-more-we-share-the-more-we-have-series-22">The More We Share, The More We Have (series 1/2)</a>" by <a href="https://thegreats.co/artists/pietro-soldi">Pietro Soldi</a> for Creative Commons &amp; Fine Acts is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a></span>
+        </figure>
+
+        <p>In search of answers, we looked at past research, notably Andrea Wallace's Barriers to Open Access — Open GLAM, and asked more than 30 experts in the open culture movement. You can watch what they told us in our CC Open Culture VOICES vlog series. Here's a small sample of what we heard</p>
+    </article>
+    
+    <article class="story">
+        <header>
+        <h3 class="title"><a href="#">Open Access in Practice: A Conversation with President Larry Kramer of The Hewlett Foundation</a></h3>
+        <span class="byline">by <a href="#">Brigitte Vezina</a>, <a href="#">Ony Anukem</a></span>
+        <span class="categories"><a href=#">Open Culture</a></span>
+    
+        </header>
+        <p>In search of answers, we looked at past research, notably Andrea Wallace's Barriers to Open Access — Open GLAM, and asked more than 30 experts in the open culture movement. You can watch what they told us in our CC Open Culture VOICES vlog series. Here's a small sample of what we heard</p>
+    </article>
+
+    <article class="story">
+        <header>
+        <h3 class="title"><a href="#">Open Access in Practice: A Conversation with President Larry Kramer of The Hewlett Foundation</a></h3>
+        <span class="byline">by <a href="#">Brigitte Vezina</a>, <a href="#">Ony Anukem</a></span>
+        <span class="categories"><a href=#">Open Culture</a></span>
+    
+        </header>
+        <p>In search of answers, we looked at past research, notably Andrea Wallace's Barriers to Open Access — Open GLAM, and asked more than 30 experts in the open culture movement. You can watch what they told us in our CC Open Culture VOICES vlog series. Here's a small sample of what we heard</p>
+    </article>
+
+    <article class="story">
+        <header>
+        <h3 class="title"><a href="#">Open Access in Practice: A Conversation with President Larry Kramer of The Hewlett Foundation</a></h3>
+        <span class="byline">by <a href="#">Brigitte Vezina</a>, <a href="#">Ony Anukem</a></span>
+        <span class="categories"><a href=#">Open Culture</a></span>
+    
+        </header>
+        <p>In search of answers, we looked at past research, notably Andrea Wallace's Barriers to Open Access — Open GLAM, and asked more than 30 experts in the open culture movement. You can watch what they told us in our CC Open Culture VOICES vlog series. Here's a small sample of what we heard</p>
+    </article>
+
+    <article class="story">
+        <header>
+        <h3 class="title"><a href="#">Open Access in Practice: A Conversation with President Larry Kramer of The Hewlett Foundation</a></h3>
+        <span class="byline">by <a href="#">Brigitte Vezina</a>, <a href="#">Ony Anukem</a></span>
+        <span class="categories"><a href=#">Open Culture</a></span>
+    
+        </header>
+        <p>In search of answers, we looked at past research, notably Andrea Wallace's Barriers to Open Access — Open GLAM, and asked more than 30 experts in the open culture movement. You can watch what they told us in our CC Open Culture VOICES vlog series. Here's a small sample of what we heard</p>
+    </article>
+    
+    <a class="more" href="#">more posts</a>
+    
+</article>
+    
+    
+
+    
 
 </main>
 
