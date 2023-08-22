e3031226330042fbef96c426f8a04825847d2512
possumbilities <109087089+possumbilities@users.noreply.github.com>
2023-08-22 19:21:00 +0000

    setup baseline skeleton import of team-index context

---
+++ src/page-team.php
@@ -0,0 +1,65 @@
+<?php get_header('', array( 'body-classes' => 'team-index') ); ?>
+
+<main>
+
+<header>
+<h1>Our Team</h1>
+
+<p>Creative Commons is a network of staff, board, emeritus, advisory council, audit committee, and affiliates around the world.</p>
+
+</header>
+
+
+<article class="persons">
+    <h2>Staff</h2>
+    <ul>
+        <li>
+            <article class="person">
+                <h3><a href="#">Mari Moreshead</a></h3>
+                <span class="title">Chief of Staff and Secretary to the Board of Directors</span>
+                <figure>
+                    <img src="imgs/profile.jpg" />
+                    <span class="attribution">by Priscilla C. Scott for Creative Commons, licensed under <a href="#">CC BY 4.0</a></span>
+                </figure>
+                <p>Mari came to CC in 2015 after three years at the Mozilla Foundation. Prior to that, she held similar administrative roles&hellip;</p>
+            </article>
+        </li>
+
+        <li>
+            <article class="person">
+                <h3><a href="#">Mari Moreshead</a></h3>
+                <span class="title">Chief of Staff and Secretary to the Board of Directors</span>
+                <figure>
+                    <img src="imgs/profile.jpg" />
+                    <span class="attribution">by Priscilla C. Scott for Creative Commons, licensed under <a href="#">CC BY 4.0</a></span>
+                </figure>
+                <p>Mari came to CC in 2015 after three years at the Mozilla Foundation. Prior to that, she held similar administrative roles&hellip;</p>
+            </article>
+        </li>
+
+       
+    </ul>
+</article>
+
+<article class="persons">
+    <h2>Board</h2>
+    <ul>
+        <li>
+            <article class="person">
+                <h3><a href="#">Mari Moreshead</a></h3>
+                <span class="title">Chief of Staff and Secretary to the Board of Directors</span>
+                <figure>
+                    <img src="imgs/profile.jpg" />
+                    <span class="attribution">by Priscilla C. Scott for Creative Commons, licensed under <a href="#">CC BY 4.0</a></span>
+                </figure>
+                <p>Mari came to CC in 2015 after three years at the Mozilla Foundation. Prior to that, she held similar administrative roles&hellip;</p>
+            </article>
+        </li>
+       
+    </ul>
+</article>
+
+
+</main>
+
+<?php get_footer(); ?>
