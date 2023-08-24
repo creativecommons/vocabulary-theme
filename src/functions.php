61b4abe57794e19bf426719fe3a74fa93673d8ef
possumbilities <109087089+possumbilities@users.noreply.github.com>
2023-08-24 19:49:21 +0000

    add filter to ensure imgs inserted into WYSIWYG editor are wrapped in figure tags, with captions

--- src/functions.php
+++ src/functions.php
@@ -10,3 +10,18 @@ function register_vocabulary_menus() {
  add_action( 'init', 'register_vocabulary_menus' );
 
  add_theme_support( 'post-thumbnails' );
+
+//  Thanks to Chris Coyier & Caspar Hübinger
+//  https://css-tricks.com/snippets/wordpress/insert-images-within-figure-element-from-media-uploader/
+//
+ function insert_image_as_figure( $html, $id, $caption, $title, $align, $url, $size, $alt, $rel ) {
+  $src  = wp_get_attachment_image_src( $id, $size, false );
+  $figure = "<figure id='post-$id media-$id' class='align-$align'>";
+  $figure .= '<img src="'.$src[0].'" alt="'.$alt.'" />';
+  if ($caption) {
+    $figure .= '<figcaption class="attribution">'.$caption.'</figcaption>';
+  }
+  $figure .= "</figure>";
+  return $figure;
+}
+add_filter( 'image_send_to_editor', 'insert_image_as_figure', 10, 9 );
