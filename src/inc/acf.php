61b186a9d7483691b06b22f3233ef34a83c8a51e
possumbilities <109087089+possumbilities@users.noreply.github.com>
2023-08-31 16:07:51 +0000

    setup program-page template, and ACF custom post type

--- src/inc/acf.php
+++ src/inc/acf.php
@@ -646,5 +646,51 @@ add_action( 'init', function() {
 	),
 	'delete_with_user' => false,
 ) );
+
+	register_post_type( 'program', array(
+	'labels' => array(
+		'name' => 'Programs',
+		'singular_name' => 'Program',
+		'menu_name' => 'Programs',
+		'all_items' => 'All Programs',
+		'edit_item' => 'Edit Program',
+		'view_item' => 'View Program',
+		'view_items' => 'View Programs',
+		'add_new_item' => 'Add New Program',
+		'new_item' => 'New Program',
+		'parent_item_colon' => 'Parent Program:',
+		'search_items' => 'Search Programs',
+		'not_found' => 'No programs found',
+		'not_found_in_trash' => 'No programs found in Trash',
+		'archives' => 'Program Archives',
+		'attributes' => 'Program Attributes',
+		'insert_into_item' => 'Insert into program',
+		'uploaded_to_this_item' => 'Uploaded to this program',
+		'filter_items_list' => 'Filter programs list',
+		'filter_by_date' => 'Filter programs by date',
+		'items_list_navigation' => 'Programs list navigation',
+		'items_list' => 'Programs list',
+		'item_published' => 'Program published.',
+		'item_published_privately' => 'Program published privately.',
+		'item_reverted_to_draft' => 'Program reverted to draft.',
+		'item_scheduled' => 'Program scheduled.',
+		'item_updated' => 'Program updated.',
+		'item_link' => 'Program Link',
+		'item_link_description' => 'A link to a program.',
+	),
+	'public' => true,
+	'hierarchical' => true,
+	'show_in_rest' => true,
+	'supports' => array(
+		0 => 'title',
+		1 => 'editor',
+		2 => 'page-attributes',
+		3 => 'thumbnail',
+	),
+	'taxonomies' => array(
+		0 => 'category',
+	),
+	'delete_with_user' => false,
+) );
 } );
 
---
+++ src/page-program.php
@@ -0,0 +1,99 @@
+<?php
+
+// if parent
+// display the program-index context
+// likely to be only 4:
+// 
+// Open Policy, 
+// Open Culture, 
+// Open Knowledge,
+// Community
+
+
+// if child
+// display the program-page context
+// likely to be many, and may include:
+// 
+// [Open Policy]
+// Copyright
+// Better Internet
+//
+// [Open Culture]
+// Open Heritage
+// Open Creativity
+//
+// [Open Knowledge]
+// Open Education
+// Open Climate
+// Open Science
+// Open Research (OA)
+// Open Journalism
+// 
+// [Community]
+// Global Network
+// Learning & Training
+// Licenses & Tools Stewardship
+// Open Source Community
+
+?>
+
+<!-- this class will need to be dynamic program-index, program-page -->
+<?php get_header('', array( 'body-classes' => 'default-page') ); ?>
+
+<main>
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+<header>
+
+<h1><?php the_title(); ?></h1>
+
+<!-- <span class="byline">by <a href="#">Marie Langley</a>, <a href="#">Marvau Laraugne</a></span> -->
+
+<?php if (!class_exists('ACF')): ?> 
+    
+<!-- display raw post_meta, if ACF not installed & activated -->
+<p><?php echo get_post_meta( get_the_ID(), 'lead_in_copy', true ); ?></p>
+
+<?php else : ?>
+
+<!-- display ACF field, if ACF installed & activated -->
+<p><?php the_field('lead_in_copy'); ?></p>
+
+<?php endif; ?>
+
+</header>
+
+<!-- <aside>
+    <nav class="contextual-menu">
+        <h2>Registration</h2>
+        <ul>
+            <li><a href="#">How many hours of work does the Certificate course require?</a></li>
+            <li><a href="#">What do I get with the Certificate?</a></li>
+        </ul>
+    </nav>
+
+    <nav>
+        <h2>Course</h2>
+        <ul>
+            <li><a href="#">Does everyone participating in the course get a certificate?</a></li>
+            <li><a href="#">Can I represent Creative Commons once I am CC Certified?</a></li>
+            <li><a href="#">How Scholarships work?</a></li>
+
+        </ul>
+    </nav>
+</aside> -->
+
+
+<div class="content">
+
+    <?php the_content(); ?>
+
+</div>
+
+<?php endwhile; // end of the loop. ?>
+
+</main>
+
+<?php get_footer(); ?>
+
