bac5cdcd50ec78db71905fe7266b7963d9b15efa
possumbilities <109087089+possumbilities@users.noreply.github.com>
2023-09-07 19:51:13 +0000

    add static page template options

---
+++ src/static-templates/static-default.php
@@ -0,0 +1,7 @@
+<!-- This file (and others like it), can be utilized to code
+bespoke pages that offer high degrees of unique customization, 
+which cannot be easily attached to the backend WordPress editor,  -->
+
+
+<p>Static default content</p>
+
