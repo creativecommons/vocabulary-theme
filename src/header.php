d5cd9cdd1a070807e3500a3012a8a9e7fb97b6d5
possumbilities <109087089+possumbilities@users.noreply.github.com>
2023-09-12 18:55:08 +0000

    output title in header.php dynamically

--- src/header.php
+++ src/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
-<title>Hello World!</title>
+<title><?php wp_title(''); echo ' | ';  bloginfo( 'name' ); ?></title>
   
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
