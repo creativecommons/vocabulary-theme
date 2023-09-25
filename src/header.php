13fac664199a8700467f05878084796f6ead9cdf
possumbilities <109087089+possumbilities@users.noreply.github.com>
2023-09-25 19:51:48 +0000

    add better output for html title on home and other pages

--- src/header.php
+++ src/header.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
-<title><?php wp_title(''); echo ' | ';  bloginfo( 'name' ); ?></title>
+<title><?php wp_title('|',true,'right'); ?><?php bloginfo('name'); ?></title>
+
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
