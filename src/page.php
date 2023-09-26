bf9cd8d29056a8bef8fc23cec8190f18564a6f79
possumbilities <109087089+possumbilities@users.noreply.github.com>
2023-09-26 14:18:27 +0000

    add logic to only display lead_in_copy acf field if content exists

--- src/page.php
+++ src/page.php
@@ -13,12 +13,16 @@
 <?php if (!class_exists('ACF')): ?>
 
 <!-- display raw post_meta, if ACF not installed & activated -->
+<?php if ( get_post_meta( get_the_ID(), 'lead_in_copy', true ) ) : ?>
 <p><?php echo get_post_meta( get_the_ID(), 'lead_in_copy', true ); ?></p>
+<?php endif; ?>
 
 <?php else : ?>
 
 <!-- display ACF field, if ACF installed & activated -->
+<?php if ( get_field('lead_in_copy') ) : ?>
 <p><?php the_field('lead_in_copy'); ?></p>
+<?php endif; ?>
 
 <?php endif; ?>
 
