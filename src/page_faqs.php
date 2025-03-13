38ab42df2e6e20777bee056e912834f8fd3395e1
possumbilities <109087089+possumbilities@users.noreply.github.com>
2025-03-13 19:04:53 +0000

    add baseline queries for all faq-index page context info needed

--- src/page_faqs.php
+++ src/page_faqs.php
@@ -38,7 +38,61 @@
 
 <div class="content">
 
-    <?php the_content(); ?>
+    <?php 
+        $topGroup = get_field('top_group');
+        $topQuestions = get_field('faqs_listing', $topGroup->ID);
+        $topClosing = get_field('closing', $topGroup->ID);
+
+
+        echo $topClosing; 
+    ?>
+
+    <h2><?php echo $topGroup->post_title; ?></h2>
+
+    <ul>
+    <?php foreach ($topQuestions as $question) : ?>
+
+        <li><?php echo $question->post_title ?></li>
+
+    <?php endforeach; ?>
+    </ul>
+
+
+    <?php if( have_rows('featured_areas') ): ?>
+    <?php while( have_rows('featured_areas') ): the_row(); 
+
+        $groups = get_sub_field('groups');
+
+    ?>
+
+        <h2>Focus Areas</h2>
+        <?php foreach ($groups as $group) : ?>
+
+            <h3><?php echo $group->post_title ?></h3>
+
+        <?php endforeach; ?>
+
+
+
+
+        <?php foreach ($groups as $group) : ?>
+
+        <h2><?php echo $group->post_title ?></h2>
+
+        <?php $questions = get_field('faqs_listing', $group->ID); ?>
+
+        <?php foreach ($questions as $question) : ?>
+
+            <h3><?php echo $question->post_title ?></h3>
+            <a href="<?php echo get_edit_post_link($question->ID); ?>">[edit]</a>
+            <?php echo apply_filters( 'the_content', $question->post_content ); ?>
+
+        <?php endforeach; ?>
+
+        <?php endforeach; ?>
+        
+    <?php endwhile; ?>
+<?php endif; ?>
 
 </div>
 
