fd04ded49a9fa18e5d149a23eae1401b4e0b1cd1
possumbilities <109087089+possumbilities@users.noreply.github.com>
2025-10-28 19:59:11 +0000

    setup pidgin dir, baseline template, baseline css file

---
+++ src/pidgin/page_home-narrative.php
@@ -0,0 +1,36 @@
+<?php /* Template Name: Index - Home Narrative */ ?>
+
+<?php
+
+get_header('', array( 'body-classes' => 'home-narrative') );
+
+?>
+
+
+<main>
+
+<article class="topic-summary intro"> 
+    <div class="description">
+        <h2>The <strong>commons</strong> belongs to all of us</h2>
+        <p>Creative Commons safeguards and strengthens the global commons, which is made up of all shared knowledge and culture, so that it can power human creativity, equity, and innovation.</p> 
+        <p>We are a global nonprofit protecting your right to access and share information and participate in knowledge.</p>
+        <ul>
+            <li><a href="#">Learn more &rarr;</a></li>
+            <li> <a href="#">Meet the team</a></li>
+            <li><a href="#">What we do</a></li>
+        </ul>
+    </div>
+
+      <figure>
+        <img src="#" />
+        <figcaption>
+            <!-- figcaption here -->
+        </figcaption>
+    </figure>
+</article>
+
+
+</main>
+
+
+<?php get_footer(); ?>
