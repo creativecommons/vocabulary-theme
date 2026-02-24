de6fb60dced4ee9c645311392b0683b16fefe330
possumbilities <109087089+possumbilities@users.noreply.github.com>
2026-02-24 16:57:34 +0000

    add baseline files and templates for pidgin work

---
+++ src/header-pidgin.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+
+<title><?php wp_title('-',true,'right'); //this requires YOAST SEO plugin to be active to output ?></title>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="<?php echo get_bloginfo( 'template_directory' ); ?>/vocabulary/favicon/favicon.ico" sizes="any" />
+<link rel="icon" href="<?php echo get_bloginfo( 'template_directory' ); ?>/vocabulary/favicon/favicon.svg" type="image/svg+xml" />
+<link rel="manifest" href="<?php echo get_bloginfo( 'template_directory' ); ?>/vocabulary/favicon/manifest.webmanifest" />
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_bloginfo( 'template_directory' ); ?>/vocabulary/favicon/apple-touch-icon.png" />
+
+<link rel="stylesheet" media="all" href="<?php echo get_bloginfo( 'template_directory' ); ?>/style.css" />
+<style>
+    /* temporary while in progress */
+    @import '<?php echo get_bloginfo( 'template_directory' ); ?>/pidgin/css/pidgin.css' layer(vocabulary);
+</style>
+
+
+<?php wp_head(); ?>
+</head>
+
+<body class="<?php echo $args['body-classes']; ?>">
+<a class="skip-to-content" href="#main-content-marker">Skip to content</a>
+
+<div class="locale icon-attach fa-globe">
+    <select id="languages-dropdown">
+        <option disabled="">Languages available</option>
+        <option data-link="/licenses/by/4.0/deed.an" id="option-an" value="an">
+        aragonés
+        </option>
+        <option data-link="/licenses/by/4.0/deed.az" id="option-az" value="az">
+        Azərbaycanca
+        </option>
+        <option data-link="/licenses/by/4.0/deed.id" id="option-id" value="id">
+        Bahasa Indonesia
+        </option>
+        <option data-link="/licenses/by/4.0/deed.eu" id="option-eu" value="eu">
+        Basque
+        </option>
+        <option data-link="/licenses/by/4.0/deed.ca" id="option-ca" value="ca">
+        català
+        </option>
+        <option data-link="/licenses/by/4.0/deed.da" id="option-da" value="da">
+        dansk
+        </option>
+        <option data-link="/licenses/by/4.0/deed.de" id="option-de" value="de">
+        Deutsch
+        </option>
+        <option data-link="/licenses/by/4.0/deed.et" id="option-et" value="et">
+        eesti
+        </option>
+        <option data-link="/licenses/by/4.0/deed.en" id="option-en" selected="" value="en">
+        English
+        </option>
+        <option data-link="/licenses/by/4.0/deed.es" id="option-es" value="es">
+        español
+        </option>
+        <option data-link="/licenses/by/4.0/deed.eo" id="option-eo" value="eo">
+        Esperanto
+        </option>
+        <option data-link="/licenses/by/4.0/deed.fr" id="option-fr" value="fr">
+        français
+        </option>
+        <option data-link="/licenses/by/4.0/deed.fy" id="option-fy" value="fy">
+        frysk
+        </option>
+        <option data-link="/licenses/by/4.0/deed.ga" id="option-ga" value="ga">
+        Gaeilge
+        </option>
+        <option data-link="/licenses/by/4.0/deed.gl" id="option-gl" value="gl">
+        galego
+        </option>
+        <option data-link="/licenses/by/4.0/deed.hr" id="option-hr" value="hr">
+        Hrvatski
+        </option>
+        <option data-link="/licenses/by/4.0/deed.it" id="option-it" value="it">
+        italiano
+        </option>
+        <option data-link="/licenses/by/4.0/deed.lv" id="option-lv" value="lv">
+        latviešu
+        </option>
+        <option data-link="/licenses/by/4.0/deed.lt" id="option-lt" value="lt">
+        Lietuviškai
+        </option>
+        <option data-link="/licenses/by/4.0/deed.hu" id="option-hu" value="hu">
+        Magyar
+        </option>
+        <option data-link="/licenses/by/4.0/deed.ms" id="option-ms" value="ms">
+        Melayu
+        </option>
+        <option data-link="/licenses/by/4.0/deed.nl" id="option-nl" value="nl">
+        Nederlands
+        </option>
+        <option data-link="/licenses/by/4.0/deed.no" id="option-no" value="no">
+        norsk
+        </option>
+        <option data-link="/licenses/by/4.0/deed.pl" id="option-pl" value="pl">
+        polski
+        </option>
+        <option data-link="/licenses/by/4.0/deed.pt" id="option-pt" value="pt">
+        Português
+        </option>
+        <option data-link="/licenses/by/4.0/deed.pt-br" id="option-pt-br" value="pt-br">
+        Português Brasileiro
+        </option>
+        <option data-link="/licenses/by/4.0/deed.ro" id="option-ro" value="ro">
+        Română
+        </option>
+        <option data-link="/licenses/by/4.0/deed.sk" id="option-sk" value="sk">
+        Slovensky
+        </option>
+        <option data-link="/licenses/by/4.0/deed.sl" id="option-sl" value="sl">
+        Slovenščina
+        </option>
+        <option data-link="/licenses/by/4.0/deed.sr-latn" id="option-sr-latn" value="sr-latn">
+        srpski (latinica)
+        </option>
+        <option data-link="/licenses/by/4.0/deed.fi" id="option-fi" value="fi">
+        suomi
+        </option>
+        <option data-link="/licenses/by/4.0/deed.sv" id="option-sv" value="sv">
+        svenska
+        </option>
+        <option data-link="/licenses/by/4.0/deed.tr" id="option-tr" value="tr">
+        Türkçe
+        </option>
+        <option data-link="/licenses/by/4.0/deed.is" id="option-is" value="is">
+        Íslenska
+        </option>
+        <option data-link="/licenses/by/4.0/deed.cs" id="option-cs" value="cs">
+        česky
+        </option>
+        <option data-link="/licenses/by/4.0/deed.el" id="option-el" value="el">
+        Ελληνικά
+        </option>
+        <option data-link="/licenses/by/4.0/deed.be" id="option-be" value="be">
+        беларуская
+        </option>
+        <option data-link="/licenses/by/4.0/deed.bg" id="option-bg" value="bg">
+        български
+        </option>
+        <option data-link="/licenses/by/4.0/deed.ru" id="option-ru" value="ru">
+        Русский
+        </option>
+        <option data-link="/licenses/by/4.0/deed.uk" id="option-uk" value="uk">
+        Українська
+        </option>
+        <option data-link="/licenses/by/4.0/deed.ar" id="option-ar" value="ar">
+        العربيّة
+        </option>
+        <option data-link="/licenses/by/4.0/deed.fa" id="option-fa" value="fa">
+        فارسی
+        </option>
+        <option data-link="/licenses/by/4.0/deed.hi" id="option-hi" value="hi">
+        हिंदी
+        </option>
+        <option data-link="/licenses/by/4.0/deed.bn" id="option-bn" value="bn">
+        বাংলা
+        </option>
+        <option data-link="/licenses/by/4.0/deed.ja" id="option-ja" value="ja">
+        日本語
+        </option>
+        <option data-link="/licenses/by/4.0/deed.zh-hans" id="option-zh-hans" value="zh-hans">
+        简体中文
+        </option>
+        <option data-link="/licenses/by/4.0/deed.zh-hant" id="option-zh-hant" value="zh-hant">
+        繁體中文
+        </option>
+        <option data-link="/licenses/by/4.0/deed.ko" id="option-ko" value="ko">
+        한국어
+        </option>
+    </select>
+</div>
+
+<header>
+    <div class="masthead">
+        <h1><a class="identity-logo" href="/">Creative Commons</a></h1>
+        <button class="expand-menu">Menu</button>
+        <!-- below menu is not final items, for testing only -->
+        <nav class="primary-menu" aria-label="Primary navigation">
+            <ul>
+                <li>
+                    <a href="#">Who We Are</a>
+                    
+                    <ul>
+                        <li><a href="#">Strategic Plan</a></li>
+                        <li><a href="#">Team</a></li>
+                        <li><a href="#">Governance</a></li>
+                        <li><a href="#">Opportunities</a></li>
+                        <li><a href="#">Press</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">What We Do</a>
+
+                    <ul>
+                        <li>
+                            <a href="#">Build <br> Open Infrastructure</a>
+                             <ul>
+                                <li><a href="#">CC Licenses</a></li>
+                                <li><a href="#">CC Signals</a></li>
+                                <li><a href="#">Public Domain</a></li>
+                                <li><a href="#">FAQs</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Implement <br> the Commons</a>
+                             <ul>
+                                <li><a href="#">Where CC Makes An Impact</a></li>
+                                <li><a href="#">Resources</a></li>
+                                <li><a href="#">Search the Commons</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Engage <br> the People</a>
+                             <ul>
+                                <li><a href="#">Training + Webinars</a></li>
+                                <li><a href="#">Advocacy</a></li>
+                                <li><a href="#">Community</a></li>
+                                <li><a href="#">Partnerships</a></li>
+                                <li><a href="#">Events</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="#">Blog</a></li>
+                <li>
+                    <a href="#">Support Us</a>
+                    <ul>
+                        <li><a href="#">Make a Gift</a></li>
+                        <li><a href="#">Ways to Give</a></li>
+                        <li><a href="#">Open Infrastructure Circle</a></li>
+                        <li><a href="#">Donor FAQ</a></li>
+                    </ul>
+                </li>
+                <li><a class="donate" href="#">Donate</a></li>
+                <!-- <li><a class="donate" href="#">Support Us</a></li> -->
+            </ul>
+        </nav>
+    </div>
+
+</header>
+
+<?php
+$noticeQuery = new WP_Query(array(
+    'post_type' => 'notice',
+    'posts_per_page' => 1,
+    'meta_key' => 'type',
+    'meta_value' => 'top-of-site'
+    //'paged' => $paged,
+));
+?>
+
+<?php if ( $noticeQuery->have_posts() ) : while ( $noticeQuery->have_posts() ) : $noticeQuery->the_post(); ?>
+<?php if (get_field('importance_level') && get_field('importance_level') != 'default') :
+    $importance_level = get_field('importance_level');
+?>
+<?php endif; ?>
+
+<article class="attention <?php echo $importance_level; ?>">
+<?php the_field('message_rich_text'); ?>
+</article>
+
+<?php endwhile; ?>
+<?php endif; ?>
+<?php wp_reset_postdata(); ?>
+
+<span id="main-content-marker"></span>
---
+++ src/single-event.php
@@ -0,0 +1,121 @@
+<?php get_header('pidgin', array( 'body-classes' => 'event-post') ); ?>
+
+<main>
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+<header>
+
+<div>
+<h1><?php the_title(); ?></h1>
+</div>
+
+<figure>
+        <!-- <img src="https://creativecommons.org/wp-content/uploads/2025/05/moon-3.jpg" class="photo" /> -->
+
+        <figcaption>
+            <p>attribution details here</p>
+            
+        </figcaption>
+    </figure>
+</header>
+
+<aside class="sidebar">
+    <article class="event-meta">
+        <h2>Date & Time</h2>
+        <p class="date">February 21, 2026</p>
+        <p class="time">2pm - 6pm</p>
+
+        <h2>Location</h2>
+
+        <p class="location">123 Example St., Ottawa ON</p>
+
+        <a href="#">Register</a>
+    </article>
+</aside>
+
+<div class="content">
+    <?php the_content(); ?>
+
+    <a href="#" class="files">Download Event Files</a>
+
+    <article class="speakers">
+        <h2>Meet the speakers</h2>
+
+        <ul>
+            <li>
+                <article class="speaker">
+                    <h3>Speaker Name</h3>
+                    <h4>Speaker Title</h4>
+
+                    <p>Short speaker bio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu maximus augue. Nulla ac velit sit amet nisi ultricies ultrices. Proin vel velit sed purus dictum eleifend vitae ut ipsum. Sed eu imperdiet ex. Nam leo erat, semper non aliquam eget, porta vitae arcu.</p>
+
+                    <figure>
+
+                        <!-- <svg class="shape1">
+                            <use href="../../../../pidgin/svg/blob3.svg"></use>
+                        </svg> -->
+
+                        <figcaption>
+                            <!-- <p>attribution details here</p> -->
+                            
+                        </figcaption>
+                    </figure>
+
+
+                </article>
+            </li>
+
+            <li>
+                <article class="speaker">
+                    <h3>Speaker Name</h3>
+                    <h4>Speaker Title</h4>
+
+                    <p>Short speaker bio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu maximus augue. Nulla ac velit sit amet nisi ultricies ultrices. Proin vel velit sed purus dictum eleifend vitae ut ipsum. Sed eu imperdiet ex. Nam leo erat, semper non aliquam eget, porta vitae arcu.</p>
+
+                    <figure>
+
+                        <!-- <svg class="shape1">
+                            <use href="../../../../pidgin/svg/blob3.svg"></use>
+                        </svg> -->
+
+                        <figcaption>
+                            <!-- <p>attribution details here</p> -->
+                            
+                        </figcaption>
+                    </figure>
+
+
+                </article>
+            </li>
+        </ul>
+    </article>
+</div>
+
+<footer>
+    <nav class="pagination">
+        <ul>
+            <li><a href="#" class="previous">Previous</a></li>
+            <li><a href="#" class="more">View All Events</a></li>
+            <li><a href="#" class="next">Next</a></li>
+        </ul>
+        
+    </nav>
+</footer>
+
+<article class="topic-summary newsletter">
+    <div class="description">
+        <h2>Get the latest CC news, and join the community to empower individuals and communities around the world.</h2>
+        <a href="#">Sign up for CC's Community Newsletter</a>
+    </div>
+    <figure>
+        <!-- <img src="https://creativecommons.org/wp-content/uploads/2023/01/Open-Palms-Not-Clutching-Fists.png"> -->
+    </figure>
+    
+</article>
+
+<?php endwhile; // end of the loop. ?>
+
+</main>
+
+<?php get_footer(); ?>
