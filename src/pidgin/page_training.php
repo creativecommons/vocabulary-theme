995f155bc8448861999b5d24f8c85576fd220223
possumbilities <109087089+possumbilities@users.noreply.github.com>
2026-02-25 21:14:15 +0000

    add training-page

---
+++ src/pidgin/page_training.php
@@ -0,0 +1,297 @@
+<?php /* Template Name: Page - Training */ ?>
+
+<?php get_header('pidgin', array( 'body-classes' => 'training-index') ); ?>
+
+<main>
+
+<header>
+
+<div>
+<h1><?php the_title(); ?></h1>
+</div>
+
+<figure>
+        <img src="<?php the_field('header_graphic') ?>" alt="" />
+
+        <figcaption>
+            <p>attribution details here</p>
+            
+        </figcaption>
+    </figure>
+</header>
+
+<article class="topic-summary about">
+    <div class="description">
+        <h2>Training and Support for the Commons</h2>
+
+        <p>Creative Commons training empowers creators, educators, researchers, and institutions to share knowledge openly and confidently. Our training programs provide the practical skills, frameworks, and tools needed to use the CC licenses effectively and to participate meaningfully in the open movement - no matter your sector or experience level.</p>
+
+        <p>Foundational understanding of open licensing and the commons is more important than ever as AI continues to change how we share online. Our training programs include up-to-date considerations on the rapidly shifting AI environment and its impacts on sharing and the commons.</p>
+
+        <a href="#">Request Training</a>
+    </div>
+
+    <figure>
+
+        <!-- <svg class="shape1">
+            <use href="../../../../pidgin/svg/blob3.svg"></use>
+        </svg> -->
+
+        <figcaption>
+            <!-- <p>attribution details here</p> -->
+            
+        </figcaption>
+    </figure>
+
+</article>
+
+
+    <blockquote>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu maximus augue. Nulla ac velit sit amet nisi ultricies ultrices. Proin vel velit sed purus dictum eleifend vitae ut ipsum. Sed eu imperdiet ex. Nam leo erat, semper non aliquam eget, porta vitae arcu. Mauris sapien mi, vestibulum in arcu quis, molestie blandit ex. Curabitur ut faucibus ex, et lacinia ex. Quisque id mauris efficitur nibh tempor maximus. Aliquam semper sapien id mauris gravida tincidunt. Ut congue et dolor eget feugiat.</p>
+    </blockquote>
+
+
+
+<article class="topic-dive">
+    <h2>Upcoming Webinars and Office Hours</h2>
+    <p>Flexible, accessible, and designed for global audiences.</p>
+
+    <article class="topic-summary focus-area">
+        <div class="description">
+
+            <p>Looking for a place to start? Our CC Certificate program is our premiere course that offers individual learners everything they need to know to start using CC licenses with confidence.</p>
+
+            <a href="#">Register Today</a>
+        </div>
+
+        <figure>
+
+            <!-- <svg class="shape1">
+                <use href="../../../../pidgin/svg/blob3.svg"></use>
+            </svg> -->
+
+            <figcaption>
+                <!-- <p>attribution details here</p> -->
+                
+            </figcaption>
+        </figure>
+
+        <article class="trainings">
+            <ul>
+                <li>
+                    <article class="training">
+                        <h3>Foundations of Creative Commons Licensing</h3>
+                        <p>An introduction to the CC licenses for beginners or teams seeking a shared baseline understanding.</p>
+
+                        <h4>You'll learn:</h4>
+
+                        <ul>
+                            <li>How CC licenses work within copyright law</li>
+                            <li>How to choose and apply a license</li>
+                            <li>What users can and cannot do with CC-licensed works</li>
+                            <li>Best practices for attribution and remixing</li>
+                        </ul>
+                        <a href="#">Learn more</a>
+                    </article>
+                </li>
+
+                <li>
+                    <article class="training">
+                        <h3>Open Education & OER Training</h3>
+                        <p>Training for schools, universities, and educators looking to unlock the power of Open Educational Resources (OER).</p>
+
+                        <h4>You'll learn:</h4>
+
+                        <ul>
+                            <li>How CC licenses work within copyright law</li>
+                            <li>How to choose and apply a license</li>
+                            <li>What users can and cannot do with CC-licensed works</li>
+                            <li>Best practices for attribution and remixing</li>
+                        </ul>
+                        <a href="#">Learn more</a>
+                    </article>
+                </li>
+
+                <li>
+                    <article class="training">
+                        <h3>Open Culture Workshops</h3>
+                        <p>Training for schools, universities, and educators looking to unlock the power of Open Educational Resources (OER).</p>
+
+                        <h4>You'll learn:</h4>
+
+                        <ul>
+                            <li>How CC licenses work within copyright law</li>
+                            <li>How to choose and apply a license</li>
+                            <li>What users can and cannot do with CC-licensed works</li>
+                            <li>Best practices for attribution and remixing</li>
+                        </ul>
+                        <a href="#">Learn more</a>
+                    </article>
+                </li>
+
+                <li>
+                    <article class="training">
+                        <h3>Open Science & Research Training</h3>
+                        <p>Training for schools, universities, and educators looking to unlock the power of Open Educational Resources (OER).</p>
+
+                        <h4>You'll learn:</h4>
+
+                        <ul>
+                            <li>How CC licenses work within copyright law</li>
+                            <li>How to choose and apply a license</li>
+                            <li>What users can and cannot do with CC-licensed works</li>
+                            <li>Best practices for attribution and remixing</li>
+                        </ul>
+                        <a href="#">Learn more</a>
+                    </article>
+                </li>
+            </ul>
+        </article>
+
+    </article>
+
+</article>
+
+<article class="topic-summary focus-area">
+    <div class="description">
+        <h2>Custom Capacity-Building Programs and Consulting</h2>
+
+        <p>For institutions and communities seeking tailored support, CC offers customizable training programs.</p>
+
+        <p>Examples include:</p>
+
+        <ul>
+            <li>How CC licenses work within copyright law</li>
+            <li>How to choose and apply a license</li>
+            <li>What users can and cannot do with CC-licensed works</li>
+            <li>Best practices for attribution and remixing</li>
+        </ul>
+
+    </div>
+
+    <figure>
+
+        <!-- <svg class="shape1">
+            <use href="../../../../pidgin/svg/blob3.svg"></use>
+        </svg> -->
+
+        <figcaption>
+            <!-- <p>attribution details here</p> -->
+            
+        </figcaption>
+    </figure>
+
+</article>
+
+
+    <blockquote>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu maximus augue. Nulla ac velit sit amet nisi ultricies ultrices. Proin vel velit sed purus dictum eleifend vitae ut ipsum. Sed eu imperdiet ex. Nam leo erat, semper non aliquam eget, porta vitae arcu. Mauris sapien mi, vestibulum in arcu quis, molestie blandit ex. Curabitur ut faucibus ex, et lacinia ex. Quisque id mauris efficitur nibh tempor maximus. Aliquam semper sapien id mauris gravida tincidunt. Ut congue et dolor eget feugiat.</p>
+    </blockquote>
+
+
+
+<article class="topic-summary focus-area">
+    <div class="description">
+        <h2>Training Formats That Fit Your Needs</h2>
+
+        <p>Examples include:</p>
+
+        <ul>
+            <li>How CC licenses work within copyright law</li>
+            <li>How to choose and apply a license</li>
+            <li>What users can and cannot do with CC-licensed works</li>
+            <li>Best practices for attribution and remixing</li>
+        </ul>
+
+    </div>
+
+    <figure>
+
+        <!-- <svg class="shape1">
+            <use href="../../../../pidgin/svg/blob3.svg"></use>
+        </svg> -->
+
+        <figcaption>
+            <!-- <p>attribution details here</p> -->
+            
+        </figcaption>
+    </figure>
+
+</article>
+
+<article class="topic-summary focus-area">
+    <div class="description">
+        <h2>Training Success Stories</h2>
+
+        <p>CC training results in real impact. Take a look at some of our past training programs and learn about their results.</p>
+
+        <a href="#">Watch the Video</a>
+
+    </div>
+
+    <figure>
+
+        <!-- <svg class="shape1">
+            <use href="../../../../pidgin/svg/blob3.svg"></use>
+        </svg> -->
+
+        <figcaption>
+            <!-- <p>attribution details here</p> -->
+            
+        </figcaption>
+    </figure>
+
+</article>
+
+<article class="topic-summary focus-area">
+    <div class="description">
+        <h2>Bring CC Training to Your Organization</h2>
+
+        <p>Creative Commons partners with institutions, governments, and networks around the world to build capacity for openness at scale.</p>
+
+        <p>To explore opportunities or request a training, contact us - we'd love to collaborate.</p>
+
+        <a href="#">Request Training</a>
+
+    </div>
+
+    <figure>
+
+        <!-- <svg class="shape1">
+            <use href="../../../../pidgin/svg/blob3.svg"></use>
+        </svg> -->
+
+        <figcaption>
+            <!-- <p>attribution details here</p> -->
+            
+        </figcaption>
+    </figure>
+
+</article>
+
+
+<article class="topic-summary focus-area">
+    <div class="description">
+        <h2>We've had the pleasure of working with some of the leading open organizations globally.</h2>
+
+    </div>
+
+    <figure>
+
+        <!-- <svg class="shape1">
+            <use href="../../../../pidgin/svg/blob3.svg"></use>
+        </svg> -->
+
+        <figcaption>
+            <!-- <p>attribution details here</p> -->
+            
+        </figcaption>
+    </figure>
+
+</article>
+
+<?php get_template_part( 'pidgin/content-partials/pidgin', 'newsletter_promo', '' ); ?>
+
+</main>
+
+<?php get_footer('pidgin'); ?>
