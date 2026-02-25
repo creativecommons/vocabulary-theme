e2751070f54354557a660b531ba3cb22392079ed
possumbilities <109087089+possumbilities@users.noreply.github.com>
2026-02-25 17:27:33 +0000

    setup person-page template

--- src/pidgin/content-partials/pidgin-single-person.php
+++ src/pidgin/content-partials/pidgin-single-person.php
@@ -1,56 +1,16 @@
-<!-- ///////////////////////////////////////////////////////////// -->
-
-<?php $devQuery = new WP_Query( array( 
-    'post_type' => 'page',
-    'pagename' => 'dev-settings' 
-    ) );
-    
-    $themeVersion = '';
-?>
-
-<?php if ( $devQuery->have_posts() ) : ?>
-<?php  while ( $devQuery->have_posts() ) : $devQuery->the_post(); ?>
-
-    <?php if( get_field('brand_version')) : ?>
-
-            <?php $themeVersion = get_field('brand_version') ?>
-
-    <?php endif; ?>
-
-<?php endwhile; ?>
-<?php endif; ?>
-<?php wp_reset_postdata(); ?>
-
-<!-- //////////////////////////////////////////////////////////// -->
-
-
-<?php if ($themeVersion == 'vocabulary2') : ?>
-
-<?php get_header('pidgin', array( 'body-classes' => 'person-page') ); ?>
-
-<?php else : ?>
-
-<?php get_header('', array( 'body-classes' => 'person-page') ); ?>
-
-<?php endif; ?>
-
-<main>
-
-<?php if ($themeVersion == 'vocabulary2') : ?>
-
-    <?php get_template_part( 'pidgin/content-partials/pidgin', 'single-person', '' ); ?>
-
-<?php else : ?>
-
 <?php while ( have_posts() ) : the_post(); ?>
 
 <header>
 
+<div>
 <h1><?php the_title(); ?></h1>
 <span class="title"><?php the_field('position_title'); ?></span>
 <?php if(get_field('pronouns') ) : ?>
 <span class="pronouns">(<?php the_field('pronouns'); ?>)</span>
 <?php endif; ?>
+<?php the_content(); ?>
+
+</div>
 
 <?php if(get_post_thumbnail_id($post_id)) : ?>
 <figure>
@@ -58,10 +18,6 @@
     <figcaption class="attribution"><?php echo get_the_post_thumbnail_caption( $post_id ); ?></figcaption>
 </figure>
 <?php endif; ?>
-
-<div class="bio">
-    <?php the_content(); ?>
-</div>
 
 </header>
 
@@ -175,17 +131,3 @@ echo paginate_links( array(
 </nav>
 
 <?php endwhile; // end of the loop. ?>
-
-<?php endif; ?>
-
-</main>
-
-<?php if ($themeVersion == 'vocabulary2') : ?>
-
-<?php get_footer('pidgin'); ?>
-
-<?php else : ?>
-
-<?php get_footer(); ?>
-
-<?php endif; ?>
