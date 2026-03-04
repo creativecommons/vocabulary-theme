f59afbfbb1011c067631fca1cc838eebf61d848c
possumbilities <109087089+possumbilities@users.noreply.github.com>
2026-03-04 19:00:06 +0000

    setup default image fallback handling

--- src/pidgin/content-partials/pidgin-page.php
+++ src/pidgin/content-partials/pidgin-page.php
@@ -7,6 +7,7 @@
 </div>
 
 <figure>
+    <?php if(get_field('header_graphic')) : ?>
     <?php $image = get_field('header_graphic'); ?>
     <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
 
@@ -14,6 +15,20 @@
         <p><?php echo $image['caption']; ?></p>
         
     </figcaption>
+
+    <?php else : ?>
+
+    <div class="default-image">
+    <img src="https://creativecommons.org/wp-content/uploads/2025/05/moon-3.jpg" class="photo" />
+    <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/pidgin/svg/blob4.svg" class="shape1" />
+    <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/pidgin/svg/blob3.svg" class="shape2" />
+    <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/pidgin/svg/repeat_c.svg" class="shape3" />
+    </div>
+    
+    <figcaption>
+        <p>Melies color Voyage dans la lune, by <a href="https://en.wikipedia.org/wiki/Georges_M%C3%A9li%C3%A8s" target="_blank" rel="noopener">Georges Méliès</a>, Public Domain.</p>
+    </figcaption>
+    <?php endif; ?>
 </figure>
 </header>
 
