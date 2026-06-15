2b43827a4640e1ada432597daab7d20ba4b73b2a
possumbilities <109087089+possumbilities@users.noreply.github.com>
2026-06-15 19:13:35 +0000

    collapse footer and header to core

--- src/header.php
+++ src/header.php
@@ -19,6 +19,156 @@
 <body class="<?php echo $args['body-classes']; ?>">
 <a class="skip-to-content" href="#main-content-marker">Skip to content</a>
 
+<div class="locale icon-attach fa-globe">
+    <select id="languages-dropdown">
+        <option disabled="">Languages available</option>
+        <option data-link="/licenses/by/4.0/deed.an" id="option-an" value="an">
+        aragonés
+        </option>
+        <option data-link="/licenses/by/4.0/deed.az" id="option-az" value="az">
+        Azərbaycanca
+        </option>
+        <option data-link="/licenses/by/4.0/deed.id" id="option-id" value="id">
+        Bahasa Indonesia
+        </option>
+        <option data-link="/licenses/by/4.0/deed.eu" id="option-eu" value="eu">
+        Basque
+        </option>
+        <option data-link="/licenses/by/4.0/deed.ca" id="option-ca" value="ca">
+        català
+        </option>
+        <option data-link="/licenses/by/4.0/deed.da" id="option-da" value="da">
+        dansk
+        </option>
+        <option data-link="/licenses/by/4.0/deed.de" id="option-de" value="de">
+        Deutsch
+        </option>
+        <option data-link="/licenses/by/4.0/deed.et" id="option-et" value="et">
+        eesti
+        </option>
+        <option data-link="/licenses/by/4.0/deed.en" id="option-en" selected="" value="en">
+        English
+        </option>
+        <option data-link="/licenses/by/4.0/deed.es" id="option-es" value="es">
+        español
+        </option>
+        <option data-link="/licenses/by/4.0/deed.eo" id="option-eo" value="eo">
+        Esperanto
+        </option>
+        <option data-link="/licenses/by/4.0/deed.fr" id="option-fr" value="fr">
+        français
+        </option>
+        <option data-link="/licenses/by/4.0/deed.fy" id="option-fy" value="fy">
+        frysk
+        </option>
+        <option data-link="/licenses/by/4.0/deed.ga" id="option-ga" value="ga">
+        Gaeilge
+        </option>
+        <option data-link="/licenses/by/4.0/deed.gl" id="option-gl" value="gl">
+        galego
+        </option>
+        <option data-link="/licenses/by/4.0/deed.hr" id="option-hr" value="hr">
+        Hrvatski
+        </option>
+        <option data-link="/licenses/by/4.0/deed.it" id="option-it" value="it">
+        italiano
+        </option>
+        <option data-link="/licenses/by/4.0/deed.lv" id="option-lv" value="lv">
+        latviešu
+        </option>
+        <option data-link="/licenses/by/4.0/deed.lt" id="option-lt" value="lt">
+        Lietuviškai
+        </option>
+        <option data-link="/licenses/by/4.0/deed.hu" id="option-hu" value="hu">
+        Magyar
+        </option>
+        <option data-link="/licenses/by/4.0/deed.ms" id="option-ms" value="ms">
+        Melayu
+        </option>
+        <option data-link="/licenses/by/4.0/deed.nl" id="option-nl" value="nl">
+        Nederlands
+        </option>
+        <option data-link="/licenses/by/4.0/deed.no" id="option-no" value="no">
+        norsk
+        </option>
+        <option data-link="/licenses/by/4.0/deed.pl" id="option-pl" value="pl">
+        polski
+        </option>
+        <option data-link="/licenses/by/4.0/deed.pt" id="option-pt" value="pt">
+        Português
+        </option>
+        <option data-link="/licenses/by/4.0/deed.pt-br" id="option-pt-br" value="pt-br">
+        Português Brasileiro
+        </option>
+        <option data-link="/licenses/by/4.0/deed.ro" id="option-ro" value="ro">
+        Română
+        </option>
+        <option data-link="/licenses/by/4.0/deed.sk" id="option-sk" value="sk">
+        Slovensky
+        </option>
+        <option data-link="/licenses/by/4.0/deed.sl" id="option-sl" value="sl">
+        Slovenščina
+        </option>
+        <option data-link="/licenses/by/4.0/deed.sr-latn" id="option-sr-latn" value="sr-latn">
+        srpski (latinica)
+        </option>
+        <option data-link="/licenses/by/4.0/deed.fi" id="option-fi" value="fi">
+        suomi
+        </option>
+        <option data-link="/licenses/by/4.0/deed.sv" id="option-sv" value="sv">
+        svenska
+        </option>
+        <option data-link="/licenses/by/4.0/deed.tr" id="option-tr" value="tr">
+        Türkçe
+        </option>
+        <option data-link="/licenses/by/4.0/deed.is" id="option-is" value="is">
+        Íslenska
+        </option>
+        <option data-link="/licenses/by/4.0/deed.cs" id="option-cs" value="cs">
+        česky
+        </option>
+        <option data-link="/licenses/by/4.0/deed.el" id="option-el" value="el">
+        Ελληνικά
+        </option>
+        <option data-link="/licenses/by/4.0/deed.be" id="option-be" value="be">
+        беларуская
+        </option>
+        <option data-link="/licenses/by/4.0/deed.bg" id="option-bg" value="bg">
+        български
+        </option>
+        <option data-link="/licenses/by/4.0/deed.ru" id="option-ru" value="ru">
+        Русский
+        </option>
+        <option data-link="/licenses/by/4.0/deed.uk" id="option-uk" value="uk">
+        Українська
+        </option>
+        <option data-link="/licenses/by/4.0/deed.ar" id="option-ar" value="ar">
+        العربيّة
+        </option>
+        <option data-link="/licenses/by/4.0/deed.fa" id="option-fa" value="fa">
+        فارسی
+        </option>
+        <option data-link="/licenses/by/4.0/deed.hi" id="option-hi" value="hi">
+        हिंदी
+        </option>
+        <option data-link="/licenses/by/4.0/deed.bn" id="option-bn" value="bn">
+        বাংলা
+        </option>
+        <option data-link="/licenses/by/4.0/deed.ja" id="option-ja" value="ja">
+        日本語
+        </option>
+        <option data-link="/licenses/by/4.0/deed.zh-hans" id="option-zh-hans" value="zh-hans">
+        简体中文
+        </option>
+        <option data-link="/licenses/by/4.0/deed.zh-hant" id="option-zh-hant" value="zh-hant">
+        繁體中文
+        </option>
+        <option data-link="/licenses/by/4.0/deed.ko" id="option-ko" value="ko">
+        한국어
+        </option>
+    </select>
+</div>
+
 <header>
     <div class="masthead">
         <h1><a class="identity-logo" href="/">Creative Commons</a></h1>
@@ -26,78 +176,78 @@
         <!-- below menu is not final items, for testing only -->
         <nav class="primary-menu" aria-label="Primary navigation">
             <ul>
-                <li><a href="/about/mission">Who We Are</a></li>
-                <li><a href="/about">What We Do</a></li>
-                <li><a href="/share-your-work">Licenses and Tools</a></li>
+                <li>
+                    <a href="/who-we-are">Who We Are</a>
+                    <button class="expand icon-replace fa-angle-down">Expand</button>
+
+                    <ul>
+                        <li><a href="/strategic-plan">Strategic Plan</a></li>
+                        <li><a href="/team">Team</a></li>
+                        <li><a href="/governance">Governance</a></li>
+                        <li><a href="/opportunities">Opportunities</a></li>
+                        <li><a href="/annual-reports/">Annual Reports & Financials</a></li>
+                        <li><a href="/history/">History</a></li>
+                        <li><a href="/press">Press</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="/what-we-do">What We Do</a>
+                    <button class="expand icon-replace fa-angle-down">Expand</button>
+
+                    <ul>
+                        <li>
+                            <a href="/build/">Build <br> Open Infrastructure</a>
+                            <button class="expand icon-replace fa-angle-down">Expand</button>
+
+                             <ul>
+                                <li><a href="/cc-licenses">CC Licenses</a></li>
+                                <li><a href="/cc-signals">CC Signals</a></li>
+                                <li><a href="/public-domain">Public Domain</a></li>
+                                <li><a href="/chooser">Chooser</a></li>
+                                <li><a href="/faq">FAQs</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="/implement/">Implement <br> the Commons</a>
+                            <button class="expand icon-replace fa-angle-down">Expand</button>
+
+                             <ul>
+                                <li><a href="/impact">Where CC Makes An Impact</a></li>
+                                <li><a href="/discover-the-commons/">Discover the Commons</a></li>
+                                <li><a href="/resources">Resources</a></li>
+                                <li><a href="https://search.creativecommons.org">Search the Commons</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="/engage/">Engage <br> the People</a>
+                            <button class="expand icon-replace fa-angle-down">Expand</button>
+
+                             <ul>
+                                <li><a href="/training-and-webinars">Training + Webinars</a></li>
+                                <li><a href="/advocacy">Advocacy</a></li>
+                                <li><a href="/community">Community</a></li>
+                                <!-- <li><a href="/partnerships">Partnerships</a></li> -->
+                                <li><a href="/events">Events</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
                 <li><a href="/blog">Blog</a></li>
-                <li><a href="/about/support-cc/">Support Us</a></li>
-                <?php
-                    $query = new WP_Query(array(
-                        'post_type' => 'notice',
-                        'posts_per_page' => 1,
-                        'meta_key' => 'type',
-                        'meta_value' => 'primary-menu'
-                        //'paged' => $paged,
-                    ));
-                ?>
+                <li>
+                    <a href="/support">Support Us</a>
+                    <button class="expand icon-replace fa-angle-down">Expand</button>
 
-                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
-
-                <li><a class="attention" href="<?php the_field('url'); ?>"><?php the_field('message'); ?></a></li>
-
-                <?php endwhile; ?>
-                <?php endif; ?>
+                    <ul>
+                        <li><a href="https://giving.gofundme.com/campaign/778218/donate?c_src=website&c_src2=makeagift">Make a Gift</a></li>
+                        <!-- <li><a href="/support/#ways-to-give">Ways to Give</a></li> -->
+                        <li><a href="/support/oic">Open Infrastructure Circle</a></li>
+                        <li><a href="/support/donor-faq">Donor FAQ</a></li>
+                    </ul>
+                </li>
+                <li><a class="donate" href="https://giving.gofundme.com/campaign/778218/donate?c_src=website&c_src2=button">Donate</a></li>
+                <!-- <li><a class="donate" href="#">Support Us</a></li> -->
             </ul>
         </nav>
-
-        <nav class="ancillary-menu" aria-label="Ancillary navigation">
-            <ul>
-                <!-- uncomment below line, if translation functionality is present on site -->
-                <!-- <li><button class="locale icon-attach fa-globe">English</button></li> -->
-                <!-- uncomment below line, if search functionality is present on site -->
-                <li><a class="search icon-attach fa-search" href="/?s">Search</a></li>
-                <li><a class="donate icon-attach fa-heart" href="https://www.classy.org/give/313412/#!/donation/checkout?c_src=website&c_src2=top-of-page-banner" target="_blank">Donate</a></li>
-                <li><button class="explore">Explore CC</button></li>
-            </ul>
-        </nav>
-    </div>
-
-
-
-    <div class="explore-panel">
-
-    <!-- (optional main CC logo, p, link on non-home site back to main site) -->
-    <!-- <aside>
-        <a class="identity-logo" href="#">Creative Commons</a>
-        <h2>Our Work Relies On You!</h2>
-        <p>Help us keep the internet free and open.</p>
-    </aside> -->
-
-    <nav class="explore-menu" aria-label="Explore menu">
-        <ul>
-            <li>
-                <a href="https://network.creativecommons.org/" target="_blank">Global Network</a>
-                <p>Join a  global community working to strengthen the Commons</p>
-            </li>
-            <li>
-                <a href="https://certificate.creativecommons.org/" target="_blank">Certificate</a>
-                <p>Become an expert in creating and engaging with openly licensed materials</p>
-            </li>
-            <li>
-                <a href="/chooser/" target="_blank">Chooser</a>
-                <p>Get help choosing the appropriate license for your work</p>
-            </li>
-            <li>
-                <a href="https://search.creativecommons.org/"  target="_blank">Search Portal</a>
-                <p>Find engines to search openly licensed material for creative and educational reuse</p>
-            </li>
-            <li>
-                <a href="https://opensource.creativecommons.org/" target="_blank">Open Source</a>
-                <p>Help us build products that maximize creativity and innovation</p>
-            </li>
-
-        </ul>
-    </nav>
     </div>
 
 </header>
@@ -119,7 +269,20 @@ $noticeQuery = new WP_Query(array(
 <?php endif; ?>
 
 <article class="attention <?php echo $importance_level; ?>">
+<div>
+<h2><?php the_field('message'); ?></h2>
 <?php the_field('message_rich_text'); ?>
+<a href="<?php the_field('link_url'); ?>"><?php the_field('link_text'); ?></a>
+</div>
+
+<figure>
+    <?php $image = get_field('graphic'); ?>
+    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+
+    <!-- <figcaption>
+        <p></p>
+    </figcaption> -->
+</figure>
 </article>
 
 <?php endwhile; ?>
@@ -127,3 +290,9 @@ $noticeQuery = new WP_Query(array(
 <?php wp_reset_postdata(); ?>
 
 <span id="main-content-marker"></span>
+
+<?php if (get_field('css_dev_hotfixes' )) : ?>
+<style>
+    <?php the_field('css_dev_hotfixes' ); ?>
+</style>
+<?php endif; ?>
